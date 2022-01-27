efbe2ccbdf55c3e880f22090773ac685cc4f9612
SlavaAndroid <slava150185@gmail.com>
2022-01-27 17:20:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/cryptoapp/adapters/CoinInfoAdapter.kt
@@ -0,0 +1,61 @@
+package com.example.cryptoapp.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cryptoapp.R
+import com.example.cryptoapp.databinding.ItemCoinInfoBinding
+import com.example.cryptoapp.pojo.CoinPriceInfo
+import com.squareup.picasso.Picasso
+
+
+class CoinInfoAdapter(private val context: Context) :
+    RecyclerView.Adapter<CoinInfoAdapter.CoinInfoViewHolder>() {
+
+    var coinInfoList: List<CoinPriceInfo> = listOf()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+    var onCoinClickListener: OnCoinClickListener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinInfoViewHolder {
+        val itemBinding =
+            ItemCoinInfoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CoinInfoViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: CoinInfoViewHolder, position: Int) {
+        val coin = coinInfoList[position]
+        with(holder) {
+            with(coin) {
+                val symbolsTemplate = context.resources.getString(R.string.symbols_template)
+                val lastUpdateTemplate = context.resources.getString(R.string.last_update_template)
+                tvSymbols.text = String.format(symbolsTemplate, fromSymbol, toSymbol)
+                tvPrice.text = price.toString()
+                tvLastUpdate.text = String.format(lastUpdateTemplate, getFormattedTime())
+                Picasso.get().load(getFullImageUrl()).into(ivLogoCoin)
+                itemView.setOnClickListener {
+                    onCoinClickListener?.onCoinClick(this)
+                }
+            }
+        }
+
+    }
+
+    override fun getItemCount() = coinInfoList.size
+
+    inner class CoinInfoViewHolder(private val binding: ItemCoinInfoBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        val ivLogoCoin = binding.ivLogoCoin
+        val tvSymbols = binding.tvSymbols
+        val tvPrice = binding.tvPrice
+        val tvLastUpdate = binding.tvLastUpdate
+    }
+
+    interface OnCoinClickListener {
+        fun onCoinClick(coinPriceInfo: CoinPriceInfo)
+    }
+}
